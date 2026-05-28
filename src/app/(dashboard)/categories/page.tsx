0a8347c748f3e608f56e1ab65d72580ee9317868
wahyuhari <wahyu.hw@gmail.com>
2026-05-28 11:41:49 +0700

    fix update categories

--- src/app/(dashboard)/categories/page.tsx
+++ src/app/(dashboard)/categories/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
